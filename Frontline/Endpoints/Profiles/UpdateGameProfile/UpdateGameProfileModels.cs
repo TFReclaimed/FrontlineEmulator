99ba34a8a024bb8080df04ef13c3024bfa5f6ffd
Nuutti <snuutti@protonmail.com>
2026-05-21 22:35:07 +0300

    Fixed selecting restricted dropship

--- Frontline/Endpoints/Profiles/UpdateGameProfile/UpdateGameProfileModels.cs
+++ Frontline/Endpoints/Profiles/UpdateGameProfile/UpdateGameProfileModels.cs
@@ -13,6 +13,6 @@ public class Validator : Validator<GameProfileUpdateRequest>
     public Validator()
     {
         RuleFor(x => x.ActiveDeckId)
-            .Must(x => x is 0 or 1 or 10 or 11);
+            .Must(x => x is 0 or 1 or 10 or 11 or 12 or 13 or 14 or 15 or 16 or 17 or 18);
     }
 }
